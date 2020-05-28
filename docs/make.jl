99c349523943964606ad2fed997a9849dbeb61f0
Henri Drake <henrifdrake@gmail.com>
2020-05-28 14:06:17 -0400

    Initial attempt at deploying docs to GH Pages

--- docs/make.jl
+++ docs/make.jl
@@ -2,5 +2,18 @@ push!(LOAD_PATH,"../src/")
 
 using Documenter, MARGO
 
-makedocs(sitename="MARGO.jl")
+makedocs(
+    sitename="MARGO.jl",
+    doctest = true,
+    authors = "Henri F. Drake",
+    format = Documenter.HTML(
+        prettyurls = get(ENV, "CI", nothing) == "true"
+    ),
+    pages = [
+      "Home" => "index.md",
+      "Installation instructions" => "installation_instructions.md",
+      "Function index" => "function_index.md"
+    ]
+)
 
+deploydocs(repo = "github.com/hdrake/MARGO.jl.git")
--- docs/src/index.md
+++ docs/src/index.md
@@ -1,2 +1,2 @@
-# Installation guide
+# MARGO.jl
 
