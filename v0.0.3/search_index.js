da41f605bf721da723baea863ac1a2973f432ff6
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-07-06 00:12:53 +0000

    build based on 102fdda

---
+++ v0.0.3/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"function_index/#Index","page":"Function index","title":"Index","text":"","category":"section"},{"location":"function_index/","page":"Function index","title":"Function index","text":"","category":"page"},{"location":"#ClimateMARGO.jl","page":"Home","title":"ClimateMARGO.jl","text":"","category":"section"},{"location":"installation_instructions/","page":"Installation instructions","title":"Installation instructions","text":"Coming soon.","category":"page"}]
+}
