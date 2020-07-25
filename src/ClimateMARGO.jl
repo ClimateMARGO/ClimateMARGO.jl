8bf0acca21cdb40b44aaf00abff6c836913991f6
Fons van der Plas <fonsvdplas@gmail.com>
2020-07-25 12:06:14 +0200

    Removed old exports

--- src/ClimateMARGO.jl
+++ src/ClimateMARGO.jl
@@ -4,11 +4,6 @@ if VERSION < v"1.3"
     @error "ClimateMARGO requires Julia v1.3 or newer."
 end
 
-export
-    ClimateModel, ClimateModelParameters, Domain, Physics, Economics, Physics,
-    optimize_controls!, save, load,
-    step_forward!
-
 include("Models/Models.jl")
 include("Diagnostics/Diagnostics.jl")
 include("Optimization/Optimization.jl")
