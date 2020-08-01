a9e825bdeec86c221818f03ed3a08080ce223327
Henri Drake <henrifdrake@gmail.com>
2020-08-01 15:39:54 -0400

    Revert "Revert "WIP: Documenting (prior to v0.1.0 release)""

--- docs/make.jl
+++ docs/make.jl
@@ -2,6 +2,20 @@ push!(LOAD_PATH,"../src/")
 
 using Documenter, ClimateMARGO
 
+pages = [
+    "Home" => "index.md",
+    "Installation instructions" => "installation_instructions.md",
+    "Submodules" => [
+        "Submodules/Domain.md",
+        "Submodules/Physics.md",
+        "Submodules/Economics.md",
+        "Submodules/Controls.md",
+    ],
+    "Diagnostics" => "diagnostics.md",
+    "Optimization" => "optimization.md",
+    "Function index" => "function_index.md"
+]
+
 makedocs(
     sitename="ClimateMARGO.jl",
     doctest = true,
@@ -9,11 +23,7 @@ makedocs(
     format = Documenter.HTML(
         prettyurls = get(ENV, "CI", nothing) == "true",
     ),
-    pages = [
-      "Home" => "index.md",
-      "Installation instructions" => "installation_instructions.md",
-      "Function index" => "function_index.md"
-    ]
+    pages = pages
 )
 
 deploydocs(repo = "github.com/hdrake/ClimateMARGO.jl.git",)
