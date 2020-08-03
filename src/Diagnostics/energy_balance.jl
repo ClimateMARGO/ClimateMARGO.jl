3ce928454bf1461c410d710135c763ba91a0a88b
Henri Drake <henrifdrake@gmail.com>
2020-08-03 18:54:01 -0400

    Added some example Docstrings

--- src/Diagnostics/energy_balance.jl
+++ src/Diagnostics/energy_balance.jl
@@ -2,36 +2,43 @@ function F(a, c0, Finf, c, G; F0=0.)
     F0 .+ a .* log.( c/c0 ) .- G*Finf
 end
 
-function F(model; M=false, R=false, G=false, F0=0.)
+"""
+    F(m::ClimateModel; M=false, R=false, G=false)
+"""
+function F(m; M=false, R=false, G=false, F0=0.)
     return F(
-        model.physics.a, model.physics.c0, model.economics.Finf,
-        c(model, M=M, R=R),
-        model.controls.geoeng .* (1. .- .~future_mask(model) * ~G),
+        m.physics.a, m.physics.c0, m.economics.Finf,
+        c(m, M=M, R=R),
+        m.controls.geoeng .* (1. .- .~future_mask(m) * ~G),
         F0=F0
     )
 end
 
 F2x(a::Float64) = a*log(2)
-F2x(model::ClimateModel) = F2x(model.physics.a)
+F2x(m::Climatem) = F2x(m.physics.a)
 
 ECS(a, B) = F2x(a)/B
-ECS(params::ClimateModelParameters) = ECS(params.physics.a, model.physics.B)
-ECS(model::ClimateModel) = ECS(model.physics.a, model.physics.B)
+ECS(params::ClimateModelParameters) = ECS(params.physics.a, m.physics.B)
+ECS(m::ClimateModel) = ECS(m.physics.a, m.physics.B)
 
 calc_B(a::Float64, ECS::Float64) = F2x(a)/ECS
 calc_B(params::ClimateModelParameters; ECS=ECS(params)) = calc_B(params.physics.a, ECS)
-calc_B(model::ClimateModel; ECS=ECS(model)) = calc_B(model.physics.a, ECS)
+calc_B(m::ClimateModel; ECS=ECS(m)) = calc_B(m.physics.a, ECS)
 
 τd(Cd, B, κ) = (Cd/B) * (B+κ)/κ
 τd(phys::Physics) = τd(phys.Cd, phys.B, phys.κ)
-τd(model::ClimateModel) = τd(model.physics)
+τd(m::ClimateModel) = τd(m.physics)
 
 T_fast(F, κ, B; A=0.) = sqrt.(1. .- A) .* F/(κ + B)
-T_fast(model::ClimateModel; M=false, R=false, G=false, A=false) = T_fast(
-    F(model, M=M, R=R, G=G),
-    model.physics.κ,
-    model.physics.B,
-    A=model.controls.adapt .* (1. .- .~future_mask(model) * ~A)
+
+"""
+    T_fast(m::ClimateModel; M=false, R=false, G=false, A=false)
+"""
+T_fast(m::ClimateModel; M=false, R=false, G=false, A=false) = T_fast(
+    F(m, M=M, R=R, G=G),
+    m.physics.κ,
+    m.physics.B,
+    A=m.controls.adapt .* (1. .- .~future_mask(m) * ~A)
 )
 
 function T_slow(F, Cd, κ, B, t, dt; A=0.)
@@ -42,28 +49,49 @@ function T_slow(F, Cd, κ, B, t, dt; A=0.)
         cumsum( (exp.( (t .- (t[1] - dt)) / τ) / τ) .* F * dt)
     )
 end
-T_slow(model::ClimateModel; M=false, R=false, G=false, A=false) = T_slow(
-    F(model, M=M, R=R, G=G),
-    model.physics.Cd,
-    model.physics.κ,
-    model.physics.B,
-    t(model),
-    model.domain.dt,
-    A=model.controls.adapt .* (1. .- .~future_mask(model) * ~A)
+
+"""
+    T_slow(m::ClimateModel; M=false, R=false, G=false, A=false)
+"""
+T_slow(m::ClimateModel; M=false, R=false, G=false, A=false) = T_slow(
+    F(m, M=M, R=R, G=G),
+    m.physics.Cd,
+    m.physics.κ,
+    m.physics.B,
+    t(m),
+    m.domain.dt,
+    A=m.controls.adapt .* (1. .- .~future_mask(m) * ~A)
 )
 
+"""
+    T(T0, F, Cd, κ, B, t, dt; A=0.)
+
+Returns the sum of the initial, fast mode, and slow mode temperature change.
+
+See also: [`T_fast`](@ref), [`T_slow`](@ref)
+"""
 T(T0, F, Cd, κ, B, t, dt; A=0.) = sqrt.(1. .- A) .* (
     T0 .+
     T_fast(F, κ, B) .+
     T_slow(F, Cd, κ, B, t, dt)
 )
-T(model::ClimateModel; M=false, R=false, G=false, A=false) = T(
-    model.physics.T0,
-    F(model, M=M, R=R, G=G),
-    model.physics.Cd,
-    model.physics.κ,
-    model.physics.B,
-    t(model),
-    model.domain.dt,
-    A=model.controls.adapt .* (1. .- .~future_mask(model) * ~A)
+
+"""
+    T(m::ClimateModel; M=false, R=false, G=false, A=false)
+
+Returns the sum of the initial, fast mode, and slow mode temperature change,
+as diagnosed from `m` and modified by the climate controls activated by the
+Boolean kwargs.
+
+See also: [`T`](@ref), [`F`](@ref)
+"""
+T(m::ClimateModel; M=false, R=false, G=false, A=false) = T(
+    m.physics.T0,
+    F(m, M=M, R=R, G=G),
+    m.physics.Cd,
+    m.physics.κ,
+    m.physics.B,
+    t(m),
+    m.domain.dt,
+    A=m.controls.adapt .* (1. .- .~future_mask(m) * ~A)
 )
