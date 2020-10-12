f0d73cde3a52962bf9a04d1fab1427cb357f9fcf
fonsp <fonsvdplas@gmail.com>
2020-10-12 14:26:25 +0000

    disable pyplot using JULIA_MARGO_LOAD_PYPLOT env variable

--- src/ClimateMARGO.jl
+++ src/ClimateMARGO.jl
@@ -10,6 +10,8 @@ include("Diagnostics/Diagnostics.jl")
 include("Optimization/Optimization.jl")
 include("IO/IO.jl")
 include("PolicyResponse/PolicyResponse.jl")
-include("Plotting/Plotting.jl")
-
+if get(ENV, "JULIA_MARGO_LOAD_PYPLOT", "1") == "1"
+    include("Plotting/Plotting.jl")
+else
+    # (in our API, we don't load the plotting functions)
 end
